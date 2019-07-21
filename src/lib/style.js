d79a3d2f0401dd496a73e7520aa4707cefb87fc7
unknown <moissonneur94@gmail.com>
2019-07-21 22:27:05 +0300

    update

---
+++ src/lib/style.js
@@ -0,0 +1,13 @@
+import { StyleSheet, Dimensions } from "react-native";
+
+export const styleSignIn = StyleSheet.create({
+  box: {
+    margin: 1,
+    height: 25,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonText:{
+    textAlign: 'center'
+  }
+});
