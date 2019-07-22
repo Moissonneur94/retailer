247263a91b06c6a6159256890939734c633e9301
unknown <moissonneur94@gmail.com>
2019-07-22 22:43:01 +0300

    Test

--- App.js
+++ App.js
@@ -1,319 +1,143 @@
+/* eslint-disable quotes */
 import React, { Component } from "react";
-import { AsyncStorage, View, Text, TextInput } from "react-native";
 import {
-  createBottomTabNavigator,
-  createAppContainer,
-  createStackNavigator
+  ActivityIndicator,
+  AsyncStorage,
+  View,
+  StatusBar,
+  ScrollView,
+  SelectionList,
+  Text
+} from "react-native";
+import {
+  createStackNavigator,
+  createSwitchNavigator,
+  createAppContainer
 } from "react-navigation";
 import { Ionicons } from "react-native-vector-icons";
-import Icon from 'react-native-vector-icons/FontAwesome';
+import Icon from "react-native-vector-icons/FontAwesome";
 import { Buffer } from "buffer";
 import { CheckBox, Button, Input } from "react-native-elements";
 
-//Авторизация
-
 class SignInScreen extends Component {
-  constructor() {
-    super();
-    this.state = {
-      checked: false,
-      userName: "",
-      userPassword: "",
-      nameValidate: true,
-      passwordValidate: true
-    };
-  }
-
-  componentDidMount = () =>
-    AsyncStorage.getItem(userName).then(value =>
-      this.setState({ userName: value })
-    );
-
-  storeData = async () => {
-    try {
-      await AsyncStorage.setItem(userName, value);
-      this.setState({ name: value });
-    } catch (error) {}
+  static navigationOptions = {
+    title: "Профиль"
   };
 
   render() {
     return (
-      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-        <Text> Вход в систему </Text>
-
-        <View style={{ marginTop: 25 }}>
-          <TextInput
-            placeholder="Email"
-            placeholderTextColor="rgba(0,0,0, 0.6)"
-            returnKeyType="next"
-            onChangeText={userName => this.setState({ userName })}
-            onSubmitEditing={() => this.passwordInput.focus()}
-            keyboardType="email-address"
-            autoCapitalize="none"
-            autoCorrect={false}
-            style={{
-              borderLeftWidth: 1,
-              borderRightWidth: 1,
-              borderTopWidth: 1,
-              borderBottomWidth: 1,
-              width: 250
-            }}
-          />
-          <TextInput
-            placeholder="Password"
-            placeholderTextColor="rgba(0,0,0, 0.6)"
-            onChangeText={userPassword => this.setState({ userPassword })}
-            returnKeyType="go"
-            secureTextEntry
-            ref={Input => (this.passwordInput = Input)}
-            style={{
-              borderLeftWidth: 1,
-              borderRightWidth: 1,
-              borderTopWidth: 1,
-              borderBottomWidth: 1,
-              marginTop: 15,
-              width: 250
-            }}
-          />
-          <CheckBox
-            title="Запомнить меня"
-            checked={this.state.checked}
-            onPress={() => this.setState({ checked: !this.state.checked })}
-          />
-          <Button
-            title="Вход"
-            onPress={() => this.props.navigation.navigate("Home")}
-          />
+      <ScrollView>
+        <View>
+          <Input label="Ваше имя" placeholder="Введите имя" />
+          <Input label="Ваша фамилия" placeholder="Введите фамилию" />
+          <Input label="Дата рождения" placeholder="Basic input" />
+          <Input label="Телефон для связи" placeholder="Введите телефон" />
+          <Input label="Эл. почта" placeholder="Введите email" />
         </View>
-      </View>
+        <View>
+          <Button title="SignIn" onPress={this._signInAsync} />
+        </View>
+      </ScrollView>
     );
   }
+
+  _signInAsync = async () => {
+    await AsyncStorage.setItem("userToken", "abc");
+    this.props.navigation.navigate("App");
+  };
 }
 
-// Главная страница
 class HomeScreen extends Component {
+  static navigationOptions = {
+    title: "Главная страница"
+  };
+
   render() {
     return (
-      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-        <Text>Приветствую1.</Text>
+      <View>
+        <SelectionList
+          renderItem={({item, index, section}) => <Text key={index}>{item}</Text>}
+          renderSectionHeader = {({section: {title}}) => (
+            <Text style={{fontWeight: 'bold'}}>{title}</Text>
+          )}
 
-        <Input
-          placeholder="INPUT WITH ICON"
-          leftIcon={{ type: "font-awesome", name: "chevron-left" }}
+          section={[
+            {title: 'Не выбрано', data:['item1', 'item2']}
+            {title: 'Жен.', data:['item3', 'item4']}
+            {title: 'Муж.', data:['item5', 'item6']}
+          ]}
+          keyExtractor = {(item, index) => item+index}
         />
+
+        <Button title="ECHO" onPress={this._showMoreApp}/>
+        <Button title="SignOut" onPress={this._signOutAsync}/>
       </View>
     );
   }
+
+  _showMoreApp = () => {
+    this.props.navigation.navigate("Other");
+  };
+
+  _signOutAsync = async () => {
+    await AsyncStorage.clear();
+    this.props.navigation.navigate("Auth");
+  };
 }
 
-// Регистрация
+class OtherScreen extends Component {
+  static navigationOptions = {
+    title: "Lots of features here"
+  };
 
-class RegistrationScreen extends Component {
+  render() {
+    return (
+      <View >
+        <Button title="I'm done, sign me out" onPress={this._signOutAsync} />
+        <StatusBar barStyle="default" />
+      </View>
+    );
+  }
+
+  _signOutAsync = async () => {
+    await AsyncStorage.clear();
+    this.props.navigation.navigate("Auth");
+  };
+}
+
+class AurhLoadingScreen extends Component {
   constructor() {
     super();
-    this.state = {
-      userEmail: "",
-      userPassword: "",
-      userName: "",
-      userLastName: "",
-      userPhone: "",
-      emailValidate: true,
-      passwordValidate: true,
-      nameValidate: true,
-      lastNameValidate: true,
-      phoneValidate: true,
-      checked: false
-    };
+    this._bootstrapAsync();
   }
 
-  // проверка на правильность ввода
-  validate(text, type) {
-    alph = /^[a-zA-Z@._-]+$/;
-    beth = /^[a-zA-Z0-9]+$/;
-    delt = /^[a-zA-Zа-яА-Я-]+$/;
-    num = /^[0-9+-]+$/;
-
-    if (type == "username") {
-      if (alph.test(text)) {
-        this.setState({
-          emailValidate: true
-        });
-      } else {
-        this.setState({
-          emailValidate: false
-        });
-      }
-    } else if (type == "password") {
-      if (beth.test(text)) {
-        this.setState({
-          passwordValidate: true
-        });
-      } else {
-        this.setState({
-          passwordValidate: false
-        });
-      }
-    } else if (type == "name") {
-      if (delt.test(text)) {
-        this.setState({
-          nameValidate: true
-        });
-      } else {
-        this.setState({
-          nameValidate: false
-        });
-      }
-    } else if (type == "lastName") {
-      if (delt.test(last_name)) {
-        this.setState({
-          lastNameValidate: true
-        });
-      } else {
-        this.setState({
-          lastNameValidate: false
-        });
-      }
-    } else if (type == "phone") {
-      if (num.test(text)) {
-        this.setState({
-          phoneValidate: true
-        });
-      } else {
-        this.setState({
-          phoneValidate: false
-        });
-      }
-    }
-  }
+  _bootstrapAsync = async () => {
+    const userToken = await AsyncStorage.getItem("userToken");
+    this.props.navigation.navigate(userToken ? "App" : "Auth");
+  };
 
   render() {
     return (
-      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-        <Text> Регистрация нового пользователя </Text>
-
-        <View style={{ marginTop: 25 }}>
-          <TextInput
-            placeholder="Фамилия"
-            placeholderTextColor="rgba(0, 0, 0, 0.6)"
-            returnKeyType="next"
-            onSubmitEditing={() => this.nameInput.focus()}
-            // onChangeText={text => this.validate(text, "lastName")}
-            onChangeText={userLastName => this.setState({ userLastName })}
-            autoCapitalize="none"
-            autoCorrect={false}
-            style={{
-              borderLeftWidth: 1,
-              borderRightWidth: 1,
-              borderTopWidth: 1,
-              borderBottomWidth: 1,
-              width: 250
-            }}
-          />
-          <TextInput
-            placeholder="Имя"
-            placeholderTextColor="rgba(0, 0, 0, 0.6)"
-            returnKeyType="next"
-            onSubmitEditing={() => this.phoneInput.focus()}
-            // onChangeText={text => this.validate(text, "name")}
-            onChangeText={userName => this.setState({ userName })}
-            autoCapitalize="none"
-            autoCorrect={false}
-            ref={input => (this.nameInput = input)}
-            style={{
-              borderLeftWidth: 1,
-              borderRightWidth: 1,
-              borderTopWidth: 1,
-              borderBottomWidth: 1,
-              marginTop: 15,
-              width: 250
-            }}
-          />
-          {/* <TextInput 
-            placeholder="Телефон"
-            placeholderTextColor="rgba(0, 0, 0, 0.6)"
-            returnKeyType="next"
-            onSubmitEditing={() => this.mailInput.focus()}
-            // onChangeText={(text) => this.validate(text, 'phone')}
-            onChangeText = {(userPhone) => this.setState({userPhone})}
-            keyboardType="phone-pad"
-            autoCapitalize="none"
-            autoCorrect={false}
-            ref={(input) => this.phoneInput = input}
-            style={{borderLeftWidth: 1, borderRightWidth: 1, borderTopWidth: 1, borderBottomWidth: 1, marginTop: 15, width: 250}}
-          /> */}
-          <TextInput
-            placeholder="Email"
-            placeholderTextColor="rgba(0, 0, 0, 0.6)"
-            returnKeyType="next"
-            onSubmitEditing={() => this.passwordInput.focus()}
-            // onChangeText={text => this.validate(text, "username")}
-            onChangeText={userEmail => this.setState({ userEmail })}
-            keyboardType="email-address"
-            autoCapitalize="none"
-            autoCorrect={false}
-            ref={input => (this.mailInput = input)}
-            style={{
-              borderLeftWidth: 1,
-              borderRightWidth: 1,
-              borderTopWidth: 1,
-              borderBottomWidth: 1,
-              marginTop: 15,
-              width: 250
-            }}
-          />
-          <TextInput
-            placeholder="Password"
-            placeholderTextColor="rgba(0, 0, 0, 0.6)"
-            // onChangeText={text => this.validate(text, "password")}
-            onChangeText={userPassword => this.setState({ userPassword })}
-            returnKeyType="go"
-            secureTextEntry
-            ref={input => (this.passwordInput = input)}
-            style={{
-              borderLeftWidth: 1,
-              borderRightWidth: 1,
-              borderTopWidth: 1,
-              borderBottomWidth: 1,
-              marginTop: 15,
-              marginBottom: 15,
-              width: 250
-            }}
-          />
-          <Button
-            title="Вход"
-            onPress={() => this.props.navigation.navigate("Home")}
-          />
-        </View>
+      <View>
+        <ActivityIndicator />
+        <StatusBar barStyle="default" />
       </View>
     );
   }
 }
 
-const SignInStack = createStackNavigator({
-  SignIn: SignInScreen,
-  Home: HomeScreen
-});
+const AppStack = createStackNavigator({ Home: HomeScreen, Other: OtherScreen });
+const AuthStack = createStackNavigator({ SignIn: SignInScreen });
 
-const RegistrationStack = createStackNavigator({
-  Registration: RegistrationScreen,
-  Home: HomeScreen
-});
-
-const TabNavigator = createBottomTabNavigator(
-  {
-    SignIn: { screen: SignInStack },
-    Registration: { screen: RegistrationStack }
-  },
-  {
-    tabBarOptions: {
-      labelStyle: {
-        fontSize: 14,
-        fontWeight: "bold"
-      },
-      activeTintColor: "tomato",
-      inactiveTintColor: "blue"
+export default createAppContainer(
+  createSwitchNavigator(
+    {
+      AurhLoading: AurhLoadingScreen,
+      App: AppStack,
+      Auth: AuthStack
+    },
+    {
+      initialRouteName: "AurhLoading"
     }
-  }
+  )
 );
-
-export default createAppContainer(TabNavigator);
--- src/lib/index.js
+++ src/lib/index.js
@@ -1,1 +1,2 @@
+/* eslint-disable quotes */
 export * from "./style";
